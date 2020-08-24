a55d59b42419c4ecba531ef16c8b6ef3208db63e
LoreSchaeffer <lm.lorenzo@outlook.it>
2020-08-24 20:55:38 +0200

    MBCore update

--- src/main/java/it/multicoredev/cookiesrain/commands/CookiesRainExecutor.java
+++ src/main/java/it/multicoredev/cookiesrain/commands/CookiesRainExecutor.java
@@ -12,9 +12,16 @@ import org.bukkit.plugin.Plugin;
 import java.util.List;
 
 /**
- * Copyright © 2020 Daniele Patella & Lorenzo Magni. All rights reserved.
+ * Copyright © 2020 by Daniele Patella & Lorenzo Magni
  * This file is part of CookiesRain.
- * Unauthorized copying, modifying, distributing of this file, via any medium is strictly prohibited.
+ * CookiesRain is under "The 3-Clause BSD License", you can find a copy <a href="https://opensource.org/licenses/BSD-3-Clause">here</a>.
+ * <p>
+ * Redistribution and use in source and binary forms, with or without modification, are permitted provided that the following conditions are met:
+ * 1. Redistributions of source code must retain the above copyright notice, this list of conditions and the following disclaimer.
+ * 2. Redistributions in binary form must reproduce the above copyright notice, this list of conditions and the following
+ * disclaimer in the documentation and/or other materials provided with the distribution.
+ * 3. Neither the name of the copyright holder nor the names of its contributors may be used to endorse or promote products
+ * derived from this software without specific prior written permission.
  * <p>
  * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING,
  * BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED.
